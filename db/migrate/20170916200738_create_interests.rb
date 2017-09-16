27c7519c58f59e09c7d2844f4bad5c08ebaf2fb1
Kevin Fang <reddacc@gmail.com>
2017-09-16 16:44:26 -0400

    new stuff

--- db/migrate/20170916200738_create_interests.rb
+++ db/migrate/20170916200738_create_interests.rb
@@ -3,7 +3,7 @@ class CreateInterests < ActiveRecord::Migration
     create_table :interests do |t|
       t.string :topics
       t.integer :time
-
+      # t.text :interested
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20170916200738) do
   create_table "interests", force: :cascade do |t|
     t.string   "topics"
     t.integer  "time"
+    t.text     "interest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
