3d2eee5aebf7269df09f381d83c998c27c067641
Kevin Fang <reddacc@gmail.com>
2017-09-17 10:45:10 -0400

    css and more

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'emilycw',
-  :password => 'password1',
-  :domain => 'yourdomain.com',
+  :user_name => 'apikey',
+  :password => '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',
+  :domain => 'apps.reddolution.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
