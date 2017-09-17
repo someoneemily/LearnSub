12f583f86b38294c25f49944603a7e958423b41f
someoneemily <iemily.cwx@gmail.com>
2017-09-17 03:56:29 -0400

    all the files for new user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,7 @@
 class UserMailer < ApplicationMailer
   default :from => 'from_zeroto_pro@example.com'
 
-  # send a signup email to the user, pass in the user object that   contains the user's email address
-  def send_daily_email(user_email, )
+  def send_daily_email(user_email)
     @data = `python3 untitled.py --q Minions --duration long`
     @data = @data.strip
     mail( :to => user_email,
--- lib/tasks/daily_mailer.rake
+++ lib/tasks/daily_mailer.rake
@@ -1,6 +1,8 @@
 namespace :daily_mailer do
   desc 'send daily list of related interest videos and topics'
   task send_daily_email: :environment do
-      UserMailer.send_daily_email("iemily.cwx@gmail.com").deliver
+  	User.all.each do |user| 
+      UserMailer.send_daily_email(user.email).deliver
+    end
   end
 end
