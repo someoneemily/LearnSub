4189545c62a5cf050e728481b6bea917c854bea9
someoneemily <iemily.cwx@gmail.com>
2017-09-17 04:14:44 -0400

    fix syntax

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,10 +2,10 @@ class UserMailer < ApplicationMailer
   default :from => 'from_zeroto_pro@example.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
-  def send_daily_email(user_email, )
+  def send_daily_email(user_email)
     @data = `python3 untitled.py --q Minions --duration long`
     @data = @data.strip
     mail( :to => user_email,
-    :subject => 'Your Daily List of Enlightenment' )
+    :subject => 'Your Daily List of Enlightenment')
   end
 end
