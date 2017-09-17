17271c7f6361d59f13207971d13e261fcbdfafce
someoneemily <iemily.cwx@gmail.com>
2017-09-17 09:38:06 -0400

    sends everyday at 9

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,18 @@
 class UserMailer < ApplicationMailer
   default :from => 'from_zeroto_pro@example.com'
 
-  def send_daily_email(user_email)
-    @data = `python3 untitled.py --q Minions --duration long`
+  def send_daily_email(user_email, interest)
+  	@time = interest.time
+  	@duration = ''
+  	if @time <= 4
+  		duration = 'short'
+  	elsif @time >= 20
+  		duration = 'long'
+  	else
+  		duration = 'medium'
+  	end
+    @data = `python3 untitled.py --q '"#{interest.topics}"' --duration '#{duration}'`
+    binding.pry
     @data = @data.strip
     mail( :to => user_email,
     :subject => 'Your Daily List of Enlightenment' )
--- lib/tasks/daily_mailer.rake
+++ lib/tasks/daily_mailer.rake
@@ -2,7 +2,9 @@ namespace :daily_mailer do
   desc 'send daily list of related interest videos and topics'
   task send_daily_email: :environment do
   	User.all.each do |user| 
-      UserMailer.send_daily_email(user.email).deliver
+  	  interests = Interest.all.where(user_id: user.id)
+      interest = interests[Random.new.rand(interests.size)]
+      UserMailer.send_daily_email(user.email, interest).deliver
     end
   end
 end
